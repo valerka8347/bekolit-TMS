0ed420fb49c44dc8eab2c92743ec4f49af30d472
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-27 15:02:19 +0300

    Initial commit

---
+++ src/test/java/com/example/bekolittms/BekolitTmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bekolittms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekolitTmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
