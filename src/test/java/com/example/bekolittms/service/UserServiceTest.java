0389ea86ac788a255c9081005870397602986eb0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-13 02:12:21 +0300

    UI

---
+++ src/test/java/com/example/bekolittms/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.bekolittms.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    void save() {
+    }
+}
